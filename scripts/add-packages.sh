c8608ad165a0021cc63bce33a50ae8ea1f3f000e
bridger <bdysonsm@utk.edu>
2017-03-22 10:24:33 -0400

    update add-packages -- add quiet flag and mysql pieces

--- scripts/add-packages.sh
+++ scripts/add-packages.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 
 ## add the following packages to our vm
-sudo apt-get install --yes default-jdk
-sudo apt-get install --yes maven
-sudo apt-get install --yes tomcat8 tomcat8-admin tomcat8-common
+sudo apt-get install --yes --quiet default-jdk
+sudo apt-get install --yes --quiet maven
+sudo apt-get install --yes --quiet tomcat8 tomcat8-admin tomcat8-common
+sudo apt-get install --yes --quiet mysql-server mysql-client

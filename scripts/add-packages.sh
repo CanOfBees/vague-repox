5c02f924db34871b2b7cf4bb3b1a08f7a729af2c
bridger <bdysonsmith@gmail.com>
2017-03-26 16:20:26 -0400

    update add-packages: add checks to make installs conditional

--- scripts/add-packages.sh
+++ scripts/add-packages.sh
@@ -8,7 +8,34 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
 
 ## add the following packages to our vm
-sudo apt-get install --yes --quiet default-jdk
-sudo apt-get install --yes --quiet maven
-sudo apt-get install --yes --quiet tomcat8 tomcat8-admin tomcat8-common
-sudo apt-get install --yes --quiet mysql-server mysql-client
+# checking for java
+if hash java 2>/dev/null; then
+	echo "** java is installed **"
+else
+	echo "** installing java **"
+	sudo apt-get install --yes --quiet default-jdk
+fi
+
+# checking for maven
+if hash mvn 2>/dev/null; then
+    echo "** maven is installed **"
+else
+    echo "** installing maven **"
+	sudo apt-get install --yes --quiet maven
+fi
+
+# checking for tomcat8
+if [ -d "/var/lib/tomcat8" ]; then
+    echo "** tomcat8 is installed **"
+else
+    echo "** installing tomcat8 **"
+	sudo apt-get install --yes --quiet tomcat8 tomcat8-admin tomcat8-common
+fi
+
+# checking for mysql
+if hash mysql 2>/dev/null; then
+    echo "** mysql is installed **"
+else
+    echo "** installing mysql **"
+	sudo apt-get install --yes --quiet mysql-server mysql-client
+fi
